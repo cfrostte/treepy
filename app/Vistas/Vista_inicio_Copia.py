0c9bc364e5388a892c67c3810b8a5361d8b0bdb5
Guillermo Becker <guille.becker.15@gmail.com>
2018-07-29 21:06:34 -0300

    varios frames

--- app/Vistas/Vista_inicio_Copia.py
+++ app/Vistas/Vista_inicio_Copia.py
@@ -13,57 +13,40 @@ actualFrame = 1
 def donothing():
 	print("donothing")
 
-def raise_frame(frame, newframe):
-    # frame.tkraise()
-    frame.pack_forget()
-    newframe.pack()
-    print("raise")
+# def dounpack(frame):
+# 	frame.pack_forget()
+# 	print("forget")
 
-def dounpack(frame):
-	frame.pack_forget()
-	print("forget")
 
-def mimenu(root, frame, newframe):
-	MENU = tk.Menu(root)
 
-	menu_archivo = tk.Menu(MENU, tearoff=0)
-	menu_edicion = tk.Menu(MENU, tearoff=0)
-	menu_vista = tk.Menu(MENU, tearoff=0)
-	menu_ayuda = tk.Menu(MENU, tearoff=0)
-
-	menu_archivo.add_command(label="Nuevo ensayo", command=donothing)
-	menu_archivo.add_separator()
-	menu_archivo.add_command(label="Salir", command=root.quit)
-
-	menu_edicion.add_command(label="Editar ensayo", command=donothing)
-	menu_edicion.add_command(label="Editar Repeticion", command=lambda:raise_frame(frame, newframe))
-
-	MENU.add_cascade(label="Archivo", menu=menu_archivo)
-	MENU.add_cascade(label="Edicion", menu=menu_edicion)
-	MENU.add_cascade(label="Vista", menu=menu_vista)
-	MENU.add_cascade(label="Ayuda", menu=menu_ayuda)
-	
-	return MENU
-
-class Menu():
-	def __init__(self, root):
-		self.filemenu = tk.Menu(root)
-		self.filemenu.add_command(label="New", command=donothing)
-		self.filemenu.add_command(label="Exit", command=donothing)
+# class Menu():
+# 	def __init__(self, root):
+# 		self.filemenu = tk.Menu(root)
+# 		self.filemenu.add_command(label="New", command=donothing)
+# 		self.filemenu.add_command(label="Exit", command=donothing)
 
 class Inicio():
 	def __init__(self):
 		self.root = tk.Tk()
 		self.root.geometry("1280x768")
 		self.root.title("TreePy Analisis de Imagenes")
-		self.frame2 = VerticalScrolledFrame(self.root)
+		self.frameActivo = "Inicio"
 
-		self.frame = VerticalScrolledFrame(self.root)
-		self.frame.pack(fill=tk.BOTH, padx=0, pady=0, expand=True)
+		misframes = ['Inicio', 'Ensayo', 'Repeticion', 'Analisis']
+		self.misframes = self.generarFrames(misframes)
+		
+		# self.frame = VerticalScrolledFrame(self.root)
+		# self.frame.set_name("FRAME DE INICIO")
+		self.misframes['Inicio'].pack(fill=tk.BOTH, padx=0, pady=0, expand=True)
+
+		# self.frame2 = VerticalScrolledFrame(self.root)
+		# self.frame2.set_name("SEGUNDO FRAME")
+
+		# self.frame.pack()
 		# raise_frame(self.frame)
 		self.root.state('zoomed')
-		self.labelUno = ttk.Label(self.frame.interior, text="Bienvenido al inicio")
-		self.labelUno2 = ttk.Label(self.frame2.interior, text="Bienvenido FRAME DOSSSS")
+		self.labelUno = ttk.Label(self.misframes['Inicio'].interior, text=self.misframes['Inicio'].nameFrame)
+		self.labelUno2 = ttk.Label(self.misframes['Ensayo'].interior, text=self.misframes['Ensayo'].nameFrame)
 		# self.labelUno.grid(row=0, column=0, sticky=tk.W)
 		# self.subframe
 		self.labelUno.pack(side=tk.TOP)
@@ -72,7 +55,7 @@ class Inicio():
 		self.ensayosRecientes = self.ensayosRecientes()
 		
 		# self.frame.grid_rowconfigure(1, minsize=10)
-		self.root.config(menu=mimenu(self.root, self.frame, self.frame2))
+		self.root.config(menu=self.mimenu(self.root))
 		self.root.mainloop()
 
 	def ensayosRecientes(self):
@@ -81,7 +64,7 @@ class Inicio():
 		colores = ['blue', 'red', 'green','black', 'yellow', 'white']
 		# rand = random.choice(colores)
 
-		frameContainer.append(tk.Frame(self.frame.interior))
+		frameContainer.append(tk.Frame(self.misframes['Inicio'].interior))
 		# frameContainer[-1].geometry("175x75")
 		frameContainer[-1].pack(side=tk.LEFT,fill=tk.BOTH, expand=True)
 
@@ -105,13 +88,54 @@ class Inicio():
 			datos.append(label)
 			datos.append(nombrelabel)
 			fotosEnsayos.append(datos)
+			label.bind("<Button-1>", leftclick)
 			if len(fotosEnsayos) % 3 ==0:
 				print("salto")
 				# rand = random.choice(colores)
-				frameContainer.append(tk.Frame(self.frame.interior))
+				frameContainer.append(tk.Frame(self.misframes['Inicio'].interior))
 				frameContainer[-1].pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 		return fotosEnsayos
 
+	def mimenu(self, root):
+		MENU = tk.Menu(root)
+
+		menu_archivo = tk.Menu(MENU, tearoff=0)
+		menu_edicion = tk.Menu(MENU, tearoff=0)
+		menu_vista = tk.Menu(MENU, tearoff=0)
+		menu_ayuda = tk.Menu(MENU, tearoff=0)
+
+		menu_archivo.add_command(label="Nuevo ensayo", command=donothing)
+		menu_archivo.add_command(label="Abrir ensayo", command=donothing)
+		menu_archivo.add_separator()
+		menu_archivo.add_command(label="Salir", command=root.quit)
+
+		menu_edicion.add_command(label="Editar Ensayo", command=lambda:self.raise_frame(self.misframes[self.frameActivo], self.misframes['Ensayo']))
+		menu_edicion.add_command(label="Editar Repeticion", command=lambda:self.raise_frame(self.misframes[self.frameActivo], self.misframes['Repeticion']))
+
+		menu_ayuda.add_command(label="Documentacion", command=donothing)
+		menu_ayuda.add_command(label="Acerca de", command=donothing)
+
+		MENU.add_cascade(label="Archivo", menu=menu_archivo)
+		MENU.add_cascade(label="Edicion", menu=menu_edicion)
+		MENU.add_cascade(label="Vista", menu=menu_vista)
+		MENU.add_cascade(label="Ayuda", menu=menu_ayuda)
+		
+		return MENU
+
+	def generarFrames(self, misframes):
+		framesGenerados = {}
+		for x in misframes:
+			frame = VerticalScrolledFrame(self.root)
+			frame.set_name(x)
+			framesGenerados[x]=frame
+		return framesGenerados
+
+	def raise_frame(self, frame, newframe):
+	    # frame.tkraise()
+	    frame.pack_forget()
+	    newframe.pack()
+	    self.frameActivo = newframe.get_name()
+	    print("raise")
 
 class DoubleScrollbarFrame(ttk.Frame):
 
@@ -146,6 +170,7 @@ class VerticalScrolledFrame(tk.Frame):
     def __init__(self, parent, *args, **kw):
         ttk.Frame.__init__(self, parent, *args, **kw)
         # create a canvas object and a vertical scrollbar for scrolling it
+        self.nameFrame = ""
         vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
         vscrollbar.pack(fill=tk.Y, side=tk.RIGHT)
         canvas = tk.Canvas(self, bd=0, highlightthickness=0,
@@ -178,10 +203,20 @@ class VerticalScrolledFrame(tk.Frame):
                 # update the inner frame's width to fill the canvas
                 canvas.itemconfigure(interior_id, width=canvas.winfo_width())
         canvas.bind('<Configure>', _configure_canvas)
+    def get_name(self):
+    	return self.nameFrame
+
+    def set_name(self, nameFrame):
+    	self.nameFrame = nameFrame
+
+def leftclick(self):
+	print("Click izquierdo")
 
 def main():
 	mi_app = Inicio()
 	return 0
+
+
 
 if __name__ == '__main__':
 	main()
