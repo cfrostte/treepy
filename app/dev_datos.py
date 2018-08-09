071f6f0392d5c128516d18c62c64b47810207a91
Carlos Frostte <carlosfrostte@gmail.com>
2018-08-08 22:12:33 -0300

    Correccion en funcion Base.buscar

--- app/dev_datos.py
+++ app/dev_datos.py
@@ -13,19 +13,23 @@ from ControladorDatos import ControladorDatos as CD
 #     for r in CD.obtener_relaciones_de(x, 'Repeticion', {'nro' : 987}):
 #         print(r)
 
-for x in CD.buscar_objetos('Ensayo', {'clave' : 1}, ['clave'], False, None):
-    print(x)
-    x.establecimiento = 3
-    x.guardar(CD.db)
+for ensayo in CD.buscar_objetos(tipo='Ensayo', filtro={'clave' : 1}, limite=1):
+    for repeticion in CD.obtener_relaciones_de(ensayo, 'Repeticion'):
+        print(repeticion)
 
-for x in CD.buscar_objetos('Ensayo', {'clave' : 2}, ['clave'], False, None):
-    print(x)
-    x.establecimiento = 4
-    x.guardar(CD.db)
-    x.eliminar(CD.db)
+# for x in CD.buscar_objetos('Ensayo', {'clave' : 1}, ['clave'], False, None):
+#     print(x)
+#     x.establecimiento = 3
+#     x.guardar(CD.db)
+
+# for x in CD.buscar_objetos('Ensayo', {'clave' : 2}, ['clave'], False, None):
+#     print(x)
+#     x.establecimiento = 4
+#     x.guardar(CD.db)
+#     x.eliminar(CD.db)
 
 # for x in CD.ultimos_modificados('Ensayo', 3):
 #     print(x)
 
-CD.exportar_informe_csv()
-CD.exportar_informe_kml()
+# CD.exportar_informe_csv()
+# CD.exportar_informe_kml()
