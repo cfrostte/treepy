039b63754b66b560d9efae42a3310413994aa390
Carlos Frostte <carlosfrostte@gmail.com>
2018-08-27 21:12:56 -0300

    Agregando calculador de puntos geograficos

---
+++ app/Datos/core/GeoEspacial.py
@@ -0,0 +1,35 @@
+"""
+Clase para realizar transformaciones afines para calcular
+puntos geograficos en base a coordenadas de pixeles
+"""
+
+import numpy as N
+
+class GeoEspacial(object):
+    def __init__(self, array=None):
+        self.trans_matrix = array
+
+    @staticmethod
+    def aumentar(a):
+        """Agregue una columna final de unos para ingresar datos"""
+        arr = N.ones((a.shape[0], a.shape[1]+1))
+        arr[:, :-1] = a
+        return arr
+
+    def transform(self, points):
+        """Transformar los datos proyectados localmente utilizando la matriz de transformación"""
+        return N.dot(GeoEspacial.aumentar(N.array(points)), self.trans_matrix)
+
+    @classmethod
+    def from_tiepoints(cls, fromCoords, toCoords):
+        """Producir transformada afín mediante la ingesta de coordenadas locales y georreferenciadas para los puntos de enlace"""
+        fromCoords = GeoEspacial.aumentar(N.array(fromCoords))
+        toCoords = N.array(toCoords)
+        trans_matrix, residuals, rank, sv = N.linalg.lstsq(fromCoords, toCoords)
+        print('residuals', residuals)
+        print('rank', rank)
+        print('sv', sv)
+        geoespacial = cls(trans_matrix) # Configurar la transformación afín de la matriz de transformación
+        solucion = N.dot(fromCoords, geoespacial.trans_matrix) # Compute la solución del modelo
+        print('solucion', solucion)
+        return geoespacial
--- app/dev_datos.py
+++ app/dev_datos.py
@@ -1,9 +1,10 @@
 """..."""
 
 from ControladorDatos import ControladorDatos as CD
+from Datos.core.GeoEspacial import GeoEspacial as GE
 
-CD.crear_estructura()
-CD.volcar_datos_prueba()
+# CD.crear_estructura()
+# CD.volcar_datos_prueba()
 # CD.respaldar_datos()
 
 # print(CD.buscar_objetos('Ensayo'))
@@ -12,36 +13,36 @@ CD.volcar_datos_prueba()
 # print(a[0])
 # print(a[0].establecimiento)
 
-ensayo = CD.crear_objeto('Ensayo')
-print(ensayo)
-print('-----------------------')
-ensayo.nro = '44444'
-ensayo.establecimiento = 'nananana'
-ensayo.nroCuadro = '5555'
-ensayo.suelo = '55555555742355532'
-ensayo.espaciamientoX = '555'
-ensayo.espaciamientoY = '5.55555'
-ensayo.plantasHa = '5555555'
-ensayo.fechaPlantacion = '01/01/5555'
-ensayo.nroTratamientos = '5555'
-ensayo.totalPlantas = ''
-ensayo.totalHas = '5555'
-ensayo.plantasParcela = '55555'
-ensayo.tipoClonal = '555555'
-ensayo.nroRepeticiones = '555555'
+# ensayo = CD.crear_objeto('Ensayo')
 # print(ensayo)
-print(ensayo)
-print('-----------------------')
-guardado = ensayo.guardar(CD.db)
-print(guardado)
-guardado.totalPlantas = '1111111'
-print('-------anten----------------')
-print(guardado)
-print('------------antes-----------')
-guardado2 =  guardado.guardar(CD.db)
+# print('-----------------------')
+# ensayo.nro = '44444'
+# ensayo.establecimiento = 'nananana'
+# ensayo.nroCuadro = '5555'
+# ensayo.suelo = '55555555742355532'
+# ensayo.espaciamientoX = '555'
+# ensayo.espaciamientoY = '5.55555'
+# ensayo.plantasHa = '5555555'
+# ensayo.fechaPlantacion = '01/01/5555'
+# ensayo.nroTratamientos = '5555'
+# ensayo.totalPlantas = ''
+# ensayo.totalHas = '5555'
+# ensayo.plantasParcela = '55555'
+# ensayo.tipoClonal = '555555'
+# ensayo.nroRepeticiones = '555555'
+# # print(ensayo)
+# print(ensayo)
+# print('-----------------------')
+# guardado = ensayo.guardar(CD.db)
+# print(guardado)
+# guardado.totalPlantas = '1111111'
+# print('-------anten----------------')
+# print(guardado)
+# print('------------antes-----------')
+# guardado2 =  guardado.guardar(CD.db)
 
-print(guardado2)
-print('-----------------------')
+# print(guardado2)
+# print('-----------------------')
 ################################################################################
 
 # for x in CD.buscar_objetos('Ensayo', {'nro' : 123}, ['clave'], False, None):
@@ -90,11 +91,24 @@ print('-----------------------')
 # CD.exportar_informe_csv()
 # CD.exportar_informe_kml()
 
-CD.exportar_informe_csv()
-CD.exportar_informe_kml()
+# CD.exportar_informe_csv()
+# CD.exportar_informe_kml()
 
 # for repeticion in CD.buscar_objetos(tipo='Repeticion', filtro={'clave' : 10}, limite=1):
 #     print(repeticion.matriz(CD.db))
 
 # for x in CD.buscar_objetos('Ensayo'):
 #     print(x)
+
+cnvSz = 1000, 600
+
+cor1 = -32.316830, -58.087159
+cor2 = -32.317897, -58.086486
+cor3 = -32.317942, -58.085295
+
+xy1 = 349, 142
+xy2 = 469, 448
+xy3 = 698, 472
+
+g = GE.from_tiepoints([xy1, xy2, xy3], [cor1, cor2, cor3])
+print(g.transform([cnvSz])[0])
