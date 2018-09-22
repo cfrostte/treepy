5b0b78e1169ba563a9852b005a5eba2369056e48
Carlos Frostte <carlosfrostte@gmail.com>
2018-09-21 21:14:17 -0300

    Testing 1 : Crear estructura antes de abrir la app

--- app/ControladorTotal.py
+++ app/ControladorTotal.py
@@ -1,37 +1,9 @@
 from ControladorDatos import ControladorDatos as CD
 from Vistas.Vista_inicio import mainInicio
-# from Vistas.Vista_inicio_Copia import mainInicio
+
+CD.crear_estructura()
 
 ultimosModificados = CD.ultimos_modificados('Ensayo', 9)
 todosLosEnsayos = CD.buscar_objetos('Ensayo')
-# print(todosLosEnsayos)
-# ultimosModificados = CD.buscar_objetos('Ensayo',None, None , False, None)
-# ensayos = []
-# repeticiones = []
-# print(ultimosModificados)
-# for ensayo in ultimosModificados:latitudCono1Label
-# 	print("+++++++++++++++++++++++++++++++++")	
-# 	# print(x)
-# 	print(ensayo)
-# 	print("+++++++++++++++++++++++++++++++++")
-# 	ensayos.append(ensayo)\ 
-# 	print("-----------------------------------")
-# 	print(CD.obtener_relaciones_de(ensayo, 'Repeticion'))
-# 	print("-----------------------------------")
-    # for repeticion in CD.obtener_relaciones_de(ensayo, 'Repeticion'):
-        # print("---------------")►
-        # print(repeticion)
-        # print("---------------")widget.master.master
 
-
-# inicio.miapp(ultimosModificados)
-
-# class getAllEnsayos(object):WPC0U495
-# 	"""docstring for getAllEnsayos"""
-# 	def __init__(self):espaciamientoYtodaLaMatriz
-# 		super(getAllEnsayos, self).__init__()
-# 		self.arg = CD.
-
-# print(CD.buscar_objetos('Repeticion', ))
-
-inicio = mainInicio(ultimosModificados, todosLosEnsayos)
+mainInicio(ultimosModificados, todosLosEnsayos)
