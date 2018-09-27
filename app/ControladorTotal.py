4600cd19c82e179f32ce92e285b6706c12cf4d66
Guille Becker <guille.becker.15@gmail.com>
2018-09-26 22:35:19 -0300

    errores menores corregidos 1

--- app/ControladorTotal.py
+++ app/ControladorTotal.py
@@ -4,34 +4,5 @@ from Vistas.Vista_inicio import mainInicio
 
 ultimosModificados = CD.ultimos_modificados('Ensayo', 9)
 todosLosEnsayos = CD.buscar_objetos('Ensayo')
-# print(todosLosEnsayos)
-# ultimosModificados = CD.buscar_objetos('Ensayo',None, None , False, None)
-# ensayos = []
-# repeticiones = []
-# print(ultimosModificados)
-# for ensayo in ultimosModificados:latitudCono1Label
-# 	print("+++++++++++++++++++++++++++++++++")	
-# 	# print(x)
-# 	print(ensayo)
-# 	print("+++++++++++++++++++++++++++++++++")
-# 	ensayos.append(ensayo)\ 
-# 	print("-----------------------------------")
-# 	print(CD.obtener_relaciones_de(ensayo, 'Repeticion'))
-# 	print("-----------------------------------")
-    # for repeticion in CD.obtener_relaciones_de(ensayo, 'Repeticion'):
-        # print("---------------")►
-        # print(repeticion)
-        # print("---------------")widget.master.master
 
-
-# inicio.miapp(ultimosModificados)
-
-# class getAllEnsayos(object):WPC0U495
-# 	"""docstring for getAllEnsayos"""
-# 	def __init__(self):espaciamientoYtodaLaMatriz
-# 		super(getAllEnsayos, self).__init__()
-# 		self.arg = CD.
-
-# print(CD.buscar_objetos('Repeticion', ))
-
-inicio = mainInicio(ultimosModificados, todosLosEnsayos)
+mainInicio(ultimosModificados, todosLosEnsayos)
